37bfb78231e1be2da8342d3cbd91aa178ab3a178
sunlele <1296648354@qq.com>
2018-11-30 16:27:56 +0800

    show index

---
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
@@ -0,0 +1,23 @@
+package com.taotao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 页面跳转Controller
+ *
+ * 分析：
+ * 请求的url:/
+ * 返回的结果：返回一个String类型，是一个逻辑视图
+ */
+@Controller
+public class PageController {
+
+    @RequestMapping("/")
+    public String showIndex(){
+        return "index";
+    }
+
+
+}
